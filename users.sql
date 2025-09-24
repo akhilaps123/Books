85c3c424ab1ebfce1ca59852b5198e96c169f59d
akhilaps123 <akhilaps1507@gmail.com>
2025-09-24 22:10:12 +0530

    updated users.sql

--- users.sql
+++ users.sql
@@ -1,4 +1,10 @@
-#INSERT INTO `users`(`id`, `name`, `city`, `score`, `bonus`, `challenge`) VALUES ('1','Raj','Chennai','88','5','fitness'),('2','Anu','Mumbai','91',NULL,'diet'),('3','Ravi','Chennai','78','3','fitness'),('4','Meena','Delhi','82',NULL,'diet'),('5','Farah','Mumbai','95','4','fitness'),('6','Kiran','Pune','70',NULL,'yoga'),('7','Latha','Pune','87',NULL,'fitness');
-#SELECT * FROM `users` WHERE score >(SELECT AVG(score) FROM `users`);
-
-#SELECT `name`  FROM `users` WHERE challenge = (SELECT challenge FROM `users` WHERE name = "Farah");
+INSERT INTO `users`(`id`, `name`, `city`, `score`, `bonus`, `challenge`) 
+VALUES ('1','Raj','Chennai','88','5','fitness'),
+('2','Anu','Mumbai','91',NULL,'diet'),
+('3','Ravi','Chennai','78','3','fitness'),
+('4','Meena','Delhi','82',NULL,'diet'),
+('5','Farah','Mumbai','95','4','fitness'),
+('6','Kiran','Pune','70',NULL,'yoga'),
+('7','Latha','Pune','87',NULL,'fitness');
+SELECT * FROM `users` WHERE score >(SELECT AVG(score) FROM `users`);
+SELECT `name`  FROM `users` WHERE challenge = (SELECT challenge FROM `users` WHERE name = "Farah");
